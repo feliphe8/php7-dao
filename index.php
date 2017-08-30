c01a2d176d3847810d155b71cab100d212432fdf
Feliphe Simões Carvalho <fsc_nfsu@hotmail.com>
2017-08-30 14:56:09 -0300

    listando usuario

--- index.php
+++ index.php
@@ -8,8 +8,23 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
-$root = new Usuario();
-$root->loadById(2);
+// Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(2);
+//echo $root;
 
-echo $root;
+// Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+// Carrega lista de usuarios procurando pelo login
+
+//$busca = Usuario::search("fe");
+//echo json_encode($busca);
+
+# Carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("feliphe", "123456");
+echo $usuario;
  ?>
